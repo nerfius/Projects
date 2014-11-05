cd00b2d10f017941086122687c81f77840c56643
TurboGurra <turbogurra@msn.com>
2014-11-05 17:18:35 +0100

    AI.cs
    uppdaterade AI

--- pazaak/pazaak/pazaak/AI.cs
+++ pazaak/pazaak/pazaak/AI.cs
@@ -9,10 +9,25 @@ namespace pazaak
     {
         public AI() : base() { }
 
-        public override void AddValueToPool() // Change this method
+        public override void PickCard() // Change this method
         {
             Random rnd = new Random();
-            cardValue += rnd.Next(1, 10);
+            int tempVal = rnd.Next(1, 10);
+            ValueCard tempCard = new ValueCard(tempVal);
+
+            if (cardValue < 11)
+            {
+                cardsOnTable.Add(tempCard);
+                cardValue += tempVal;
+            }
+            else
+                if (rnd.Next(11, 20) > cardValue)
+                {
+                    cardsOnTable.Add(tempCard); // annars så räknas det som en stand
+                    cardValue += tempVal;
+                }
+
+            //cardValue += rnd.Next(1, 10);
         }
     }
 }
